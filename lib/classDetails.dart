c062c9de1b0f2c078843567eb0fc4b52cfbf4802
gobi5998 <gobinathvishnupriya@gmail.com>
2024-07-16 11:43:57 +0530

    first

---
+++ lib/classDetails.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'bottoncontainer.dart';
+
+
+     class Classdetails extends StatelessWidget {
+       const Classdetails({super.key});
+
+       @override
+       Widget build(BuildContext context) {
+         return Scaffold(
+           body: SafeArea(
+             child:
+             BottomContainer(
+               ),
+           ),
+
+           );
+       }
+     }
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+\port 'package:flutter/material.dart';
+||port 'classDetails.dart';
+void main(){
+  runApp(Myapp());
+}
+class Myapp extends StatefulWidget {
+  const Myapp({super.key});
+
+  @override
+  State<Myapp> createState() => _MyappState();
+}
+
+class _MyappState extends State<Myapp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+    home: Classdetails(),);
+  }
+}
+
+}
+class Myapp extends StatefulWidget {
+  const Myapp({super.key});
+
+  @override
+  State<Myapp> createState() => _MyappState();
+}
+
+class _MyappState extends State<Myapp> {
+  @override
+  Widget build(BuildContext context) {
+    rxturn const MaterialApp(
+    home: Classdetails(),);
+  }
+}
